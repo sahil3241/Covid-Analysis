cf8aec212c014470185919cc50ee340009769c1e
Sahil <sahil06202@gmail.com>
2024-03-23 09:37:19 +0530

    added navbar

--- static/index.js
+++ static/index.js
@@ -4,12 +4,31 @@ const secondDoseMenuItem = document.getElementById('second');
 
 const imageContainer = document.getElementById('image-container');
 
+const imagePlaceholder = document.getElementById('image-placeholder');
+
+imageContainer.style.display = 'none';
+
 firstDoseMenuItem.addEventListener('click', function() {
-    // Update the image source to the first dose image
-    imageContainer.innerHTML = '<img src="' + image1Url + '" alt="First Dose Image">';
+
+    if(imageContainer.style.display=='none'){
+        imageContainer.style.display='flex';
+        imageContainer.innerHTML = '<img src="' + image1Url + '" alt="First Dose Image">';
+        imagePlaceholder.style.width = '700px';
+    }
+    if(imageContainer.style.display=='flex'){
+        imageContainer.innerHTML = '<img src="' + image1Url + '" alt="Second Dose Image">';
+        imagePlaceholder.style.width = '700px';
+    }
+    
 });
 
 secondDoseMenuItem.addEventListener('click', function() {
-    // Update the image source to the second dose image
-    imageContainer.innerHTML = '<img src="' + image2Url + '" alt="Second Dose Image">';
+
+    if(imageContainer.style.display=='none'){
+        imageContainer.style.display='flex';
+        imageContainer.innerHTML = '<img src="' + image2Url + '" alt="Second Dose Image">';
+    }
+    if(imageContainer.style.display=='flex'){
+        imageContainer.innerHTML = '<img src="' + image2Url + '" alt="Second Dose Image">';
+    }
 });
